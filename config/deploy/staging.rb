69bfa6d2257288c49d694786503660f4ef6396cf
Peter Dang <peter@rubify.com>
2013-04-05 13:38:04 +0700

    first commit

---
+++ config/deploy/staging.rb
@@ -0,0 +1,6 @@
+server "192.168.1.35", :app, :web, :db, :primary => true
+set :rails_env, "staging"
+set :user, 'rubifyvn'
+set :branch, :master
+set :deploy_to, "/home/rubifyvn/www/ejweb_deployment"
+set :port, 22
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ejweb::Application.config.secret_token = '03f5c849820bcc8e9c4fcd7e976065dacccba328c852baba603beb412cfd034c3e7d4d04bb52eae509a121180641b63bfa1e824571bff1be75eb998dbd5f19bb'
